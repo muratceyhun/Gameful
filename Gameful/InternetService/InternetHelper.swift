14e7dfb6c163989bbce93d9103f4d4884647eb35
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-01-16 22:15:34 +0300

    Network staff is done

---
+++ Gameful/InternetService/InternetHelper.swift
@@ -0,0 +1,34 @@
+//
+//  InternetHelper.swift
+//  Gameful
+//
+//  Created by Murat Ceyhun Korpeoglu on 16.01.2023.
+//
+
+import Foundation
+
+enum ErrorTypes: String, Error {
+    case invalidData = "Invalid data!!"
+    case invalidURL = "Invalid url!!"
+    case generalError = "An error detected!!"
+}
+
+class InternetHelper {
+    
+    // All games url address regardless of category.
+    // https://api.rawg.io/api/games?key=2bc9a39de38844e0afee58925b2a3480
+    
+    static let shared = InternetHelper()
+    private let baseURL = "https://api.rawg.io/api/"
+    private let apiKey = "2bc9a39de38844e0afee58925b2a3480"
+    private let filterGames = "games?key="
+    private let filterPlatforms = "platforms?key="
+    
+    func requestUrl(url: String) -> String {
+        baseURL + filterGames + apiKey
+    }
+    
+    func getImagePath(url: String) -> () {
+        //TODO:
+    }
+}
---
+++ Gameful/InternetService/InternetManager.swift
@@ -0,0 +1,41 @@
+//
+//  InternetManager.swift
+//  Gameful
+//
+//  Created by Murat Ceyhun Korpeoglu on 16.01.2023.
+//
+
+import Alamofire
+
+class InternetManager {
+    
+    
+    
+    static let shared = InternetManager()
+    
+    func request<T: Codable>(type: T.Type,
+                             url: String,
+                             method: HTTPMethod,
+                             completion: @escaping((Result<T, ErrorTypes>)->())) {
+        AF.request(url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "", method: method).responseData { response in
+            switch response.result {
+            case .success(let data):
+                self.handleResponse(data: data) { response in
+                    completion(response)
+                }
+            case .failure(let _):
+                completion(.failure(.generalError))
+            }
+        }
+    }
+    
+    fileprivate func handleResponse<T: Codable>(data: Data, completion: @escaping((Result<T, ErrorTypes>)->())) {
+        do {
+            let result = try JSONDecoder().decode(T.self, from: data)
+            completion(.success(result))
+        } catch {
+            completion(.failure(.invalidData))
+        }
+    }
+}
+
