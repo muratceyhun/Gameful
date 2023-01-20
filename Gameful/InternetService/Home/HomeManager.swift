094d0e2895dd6d5358d8bc373f10273bcc9bc091
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-01-20 11:47:06 +0300

    Model file created for Game

--- Gameful/InternetService/Home/HomeManager.swift
+++ Gameful/InternetService/Home/HomeManager.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol HomeManagerProtocol {
+    func getAllGames(complete: @escaping(()->()))
+}
+
+class HomeManager: HomeManagerProtocol {
+    static let shared = HomeManager()
+    func getAllGames(complete: @escaping (() -> ())) {
+        
+    }
+    
+    
+}
+
+
+
